90d919843fbaa46f0013f0e01b3188377f7e74d3
mewmewmewmewmewmewmew <mr.ponsers@gmail.com>
2024-05-13 13:07:47 -0400

    add

--- Hook Drill/Assets/Scenes/DrillDataScriptableObject.cs
+++ Hook Drill/Assets/Scenes/DrillDataScriptableObject.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class DrillDataScriptableObject : ScriptableObject
 {
-    [Range(0f, 1f)]
+    [Range(0f, 100f)]
     [SerializeField] float AirDecceleration;
-    [Range(0f, 1f)]
+    [Range(0f, 100f)]
     [SerializeField] float AirTurningMaxSpeed;
-    [Range(0f, 1f)]
+    [Range(0f, 100)]
     [SerializeField] float GroundAcceleration;
-    [Range(0f, 1f)]
+    [Range(0f, 100f)]
     [SerializeField] float GroundTurningMaxSpeed;
-    [Range(0f, 1f)]
+    [Range(-10f, 100f)]
     [SerializeField] float Gravity;
+    [Range(0f, 100f)]
+    [SerializeField] float RopeLength;
+    [Range(0, 10)]
+    [SerializeField] int RopeNumberOfJoints;
+    [Range(0f, 100f)]
+    [SerializeField] float SpeedBoostDuration;
+    [Range(0f, 100f)]
+    [SerializeField] float SpeedBoostForce;
+    [Range(0f, 100f)]
+    [SerializeField] float SpeedBoostCoolDown;
+
 
 
 }
