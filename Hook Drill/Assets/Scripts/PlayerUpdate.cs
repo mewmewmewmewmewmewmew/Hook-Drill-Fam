a4ffecebff0c182a3431e07244615f246abc0069
Malo Sadoine <m.sadoine@student.isartdigital.com>
2024-05-20 18:29:36 -0400

    Update the player
    Create the Rope boost and the Camera view

--- Hook Drill/Assets/Scripts/PlayerUpdate.cs
+++ Hook Drill/Assets/Scripts/PlayerUpdate.cs
@@ -1,32 +1,52 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerUpdate : MonoBehaviour
 {
     public PlayerScriptableObject _playerValues;
+    public CameraObject _cameraValues;
+
+    public Camera cam;
+
     public BoxCollider2D _playerCollider;
+
     public GameObject Objdirection;
     public GameObject Hook;
 
     public Text SpeedText;
-    public Text TurningLeftTimeText;
-    public Text TurningRightTimeText;
+    public Text TurningTimeText;
 
     private bool isGrounded;
     private bool DrillMode;
     private bool isInGround;
-    private bool turningLeft;
-    private bool turningRight;
+    private bool DirectionTurn;
+    private bool isBoosting;
 
     private float changeTime;
     private float currentSpeed;
     private float prevAngle;
-    private float turningRightTime;
-    private float turningLeftTime;
+    private float turningTime;
+    private float boostAccelerationUpdtated;
+    private float BoostTime;
+    private float currentZoom;
+    private float velocity;
+    private float zoomMultiplier;
 
     private Vector3 _playerVelocity;
     private Vector2 joyPos;
     private Vector2 PrevJoyPos;
+
+    private void UpdateCamera()
+    {
+        this.currentZoom = this.currentSpeed * this.zoomMultiplier;
+        if(!this.isBoosting ) 
+            this.currentZoom = Mathf.Clamp(this.currentZoom, this._cameraValues.minimumZoom, this._cameraValues.maximumZoom);
+
+        this.cam.orthographicSize = Mathf.SmoothDamp(this.cam.orthographicSize, this.currentZoom, ref this.velocity, this._cameraValues.smoothTime);
+        this.cam.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -10);
+    }
+
     private void UpdateNoDrill()
     {
         if (this.isGrounded) 
@@ -81,28 +101,31 @@ public class PlayerUpdate : MonoBehaviour
 
         if(angle < 0)
         {
-            this.turningLeft = true;
-            this.turningRight = false;
-            this.turningRightTime = 0;
+            if(!DirectionTurn)
+                this.turningTime = 0;
+
+            DirectionTurn = true;
+            this.turningTime += Time.deltaTime;
         }
         else if(angle > 0)
         {
-            this.turningRight= true;
-            this.turningLeft = false;
-            this.turningLeftTime = 0;
+            if(DirectionTurn)
+                this.turningTime = 0;
+
+            DirectionTurn = false;
+            this.turningTime += Time.deltaTime;
         }
-        else if (angle == 0)
-        {
-            this.turningLeft= false;
-            this.turningRight= false;
-        }
+
 
         this.PrevJoyPos = this.joyPos;
     }
     private void UpdateWithDrill()
     {
-        if (this.currentSpeed > this._playerValues.maxSpeedInGround)
+        if (!this.isBoosting && this.currentSpeed > this._playerValues.maxSpeedInGround)
             this.currentSpeed = this._playerValues.maxSpeedInGround;
+
+        if (this.isBoosting && this.BoostTime < this._playerValues.boostTimeLimit)
+            this.currentSpeed *= this.boostAccelerationUpdtated;
 
         if (this.isInGround) { this.UpdateInGroundDrill(); }
         else { this.UpdateOutofGroundDrill(); }
@@ -112,9 +135,7 @@ public class PlayerUpdate : MonoBehaviour
         this.CheckTheTurn();
 
         if (this.isInGround) 
-        { 
             this._playerVelocity = transform.TransformDirection(Vector2.down) * this.currentSpeed;
-        }
 
         this.transform.position += this._playerVelocity * Time.deltaTime;
     }
@@ -156,21 +177,25 @@ public class PlayerUpdate : MonoBehaviour
         if (this.changeTime <= this._playerValues.changeTimeLimit + 0.5f)
             this.changeTime += Time.deltaTime;
 
-        if (turningLeft)
-            this.turningLeftTime += Time.deltaTime;
+        if (this.isBoosting && this.BoostTime < this._playerValues.boostTimeLimit + 0.5f)
+            this.BoostTime += Time.deltaTime;
 
-        if(turningRight)
-            this.turningRightTime += Time.deltaTime;
+        if (!this.isBoosting)
+            this.BoostTime = 0;
+
+        if(this.BoostTime > this._playerValues.boostTimeLimit)
+            this.isBoosting = false;
     }
     void Start()    
     {
         Physics2D.IgnoreLayerCollision(7, 3, true);
-        Physics2D.IgnoreLayerCollision(6, 7, true);
 
         this.currentSpeed = this._playerValues.speed;
         this.isGrounded = false;
         this.prevAngle = -90;
         this.Hook.SetActive(false);
+        this.currentZoom = this._cameraValues.maximumZoom;
+        this.zoomMultiplier = this._playerValues.minSpeedInGround / this._cameraValues.minimumZoom;
     }
     void Update()
     {
@@ -178,21 +203,33 @@ public class PlayerUpdate : MonoBehaviour
         this.inputHandler();
 
         this.SpeedText.text = "Speed : " + string.Format("{0:0.00}", this.currentSpeed);
-        this.TurningLeftTimeText.text = "LeftTime : " + string.Format("{0:0.00}", this.turningLeftTime);
-        this.TurningRightTimeText.text = "RightTime : " + string.Format("{0:0.00}", this.turningRightTime);
+        this.TurningTimeText.text = "TurningTime : " + string.Format("{0:0.00}", this.turningTime);
 
         if (!this.DrillMode) { this.UpdateNoDrill(); } 
 
         if (this.DrillMode) { this.UpdateWithDrill(); }
+
+        this.UpdateCamera();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("ENTER " + collision.tag);
-
-        if(this.DrillMode) 
+        
+        if (this.DrillMode && collision.CompareTag("Ground")) 
         { 
             this.isInGround = true;
             collision.isTrigger = true;
+        }
+        else if (this.DrillMode && collision.CompareTag("Rope") && !isBoosting)
+        {
+            this.isBoosting = true;
+            float ratio =  this.turningTime / this._playerValues.MaxLoopTime;
+
+            if (ratio > 1) { ratio = 1; }
+
+            this.boostAccelerationUpdtated = (this._playerValues.MaxboostAcceleration - 1) * ratio;
+
+            this.boostAccelerationUpdtated += 1;
         }
         else if (!this.DrillMode) { Debug.Log(" NO DRILL"); }
     }
@@ -200,7 +237,7 @@ public class PlayerUpdate : MonoBehaviour
     {
         Debug.Log("EXIT " + collision.tag);
 
-        if (this.DrillMode) 
+        if (this.DrillMode && collision.CompareTag("Ground")) 
         {
             this.isInGround = false;
             collision.isTrigger = false;
@@ -209,7 +246,6 @@ public class PlayerUpdate : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("COLLIDER");
         if (this.DrillMode) 
         { 
             Debug.Log("DRILL"); 
